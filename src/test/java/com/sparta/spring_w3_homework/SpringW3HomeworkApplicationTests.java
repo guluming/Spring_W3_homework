c4fa660d4d49c68705054887748d1fd8fccbf4c8
guluming <guluming@naver.com>
2022-05-22 21:45:02 +0900

    설정 파일

---
+++ src/test/java/com/sparta/spring_w3_homework/SpringW3HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_w3_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringW3HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
