42825c4797bb1cf8c3e23598da290c72ea679d9d
guluming <guluming@naver.com>
2022-05-24 15:07:13 +0900

    게시물 CRUD 기능 구현

--- src/main/java/com/sparta/spring_w3_homework/service/NoticeboardService.java
+++ src/main/java/com/sparta/spring_w3_homework/service/NoticeboardService.java
@@ -3,10 +3,14 @@ package com.sparta.spring_w3_homework.service;
 import com.sparta.spring_w3_homework.domain.Noticeboard;
 import com.sparta.spring_w3_homework.domain.NoticeboardRepository;
 import com.sparta.spring_w3_homework.domain.NoticeboardRequestDto;
+import com.sparta.spring_w3_homework.domain.NoticeboardResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -14,22 +18,39 @@ public class NoticeboardService {
 
     private final NoticeboardRepository noticeboardRepository;
 
+    //게시글 저장
+    @Transactional
+    public Long save(NoticeboardRequestDto params){
+        Noticeboard entity = noticeboardRepository.save(params.toEntity());
+        return entity.getId();
+    }
+
+    //게시글 전체 조회
+    public List<NoticeboardResponseDto> findAll(){
+        Sort sort = Sort.by(Sort.Direction.DESC, "id", "modifiedDate");
+        List<Noticeboard> list = noticeboardRepository.findAll(sort);
+        return list.stream().map(NoticeboardResponseDto::new).collect(Collectors.toList());
+    }
+
     //게시글 수정
     @Transactional
-    public Long update(Long id, NoticeboardRequestDto requestDto){
-        Noticeboard noticeboard = noticeboardRepository.findById(id).orElseThrow(
-                () -> new NullPointerException("존재하지 않는 사용자 입니다.")
-        );
-        noticeboard.update(requestDto);
-        return noticeboard.getId();
+    public Long update(final Long id, final NoticeboardRequestDto params){
+        Noticeboard entity = noticeboardRepository.findById(id).orElseThrow(() -> new NullPointerException("존재하지 않는 사용자 입니다."));
+        entity.update(params.getUsername(), params.getTitle(), params.getContents());
+        return id;
     }
 
     //게시글 상세 정보
     @Transactional
-    public NoticeboardRequestDto findById(Long id){
-        Noticeboard noticeboard = noticeboardRepository.findById(id).orElseThrow(
-                () -> new NullPointerException("존재하지 않는 게시글 입니다")
-        );
-        return new NoticeboardRequestDto();
+    public NoticeboardResponseDto findById(Long id){
+        Noticeboard entity = noticeboardRepository.findById(id).orElseThrow(() -> new NullPointerException("존재하지 않는 게시글 입니다."));
+        return new NoticeboardResponseDto(entity);
+    }
+
+    @Transactional
+    public Long delete(Long id){
+        Noticeboard entity = noticeboardRepository.findById(id).orElseThrow(()->new NullPointerException("존재하지 않는 게시글 입니다."));
+        entity.delete();
+        return id;
     }
 }
