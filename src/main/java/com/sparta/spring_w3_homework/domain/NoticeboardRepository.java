3a2f131fe33a3b2c9521bd2d282c9706a1a9e819
guluming <guluming@naver.com>
2022-05-25 00:53:47 +0900

    시간체크 함수 생성

--- src/main/java/com/sparta/spring_w3_homework/domain/NoticeboardRepository.java
+++ src/main/java/com/sparta/spring_w3_homework/domain/NoticeboardRepository.java
@@ -1,10 +1,10 @@
 package com.sparta.spring_w3_homework.domain;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface NoticeboardRepository extends JpaRepository<Noticeboard, Long> {
-    List<Noticeboard> findAllByDeleteYn(final char deleteYn, final Sort sort);
+    List<Noticeboard> findAllByModifiedAtBetweenOrderByModifiedAtDesc(LocalDateTime startDatetime, LocalDateTime endDatetime);
 }
