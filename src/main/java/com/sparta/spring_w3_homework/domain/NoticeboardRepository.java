a8623772c54780e4ef98ce4a86e480c330a2e76b
guluming <guluming@naver.com>
2022-05-24 20:38:58 +0900

    삭제 후 게시물 전체 조회 추가

--- src/main/java/com/sparta/spring_w3_homework/domain/NoticeboardRepository.java
+++ src/main/java/com/sparta/spring_w3_homework/domain/NoticeboardRepository.java
@@ -1,9 +1,10 @@
 package com.sparta.spring_w3_homework.domain;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface NoticeboardRepository extends JpaRepository<Noticeboard, Long> {
-    List<Noticeboard> findAllByOrderByModifiedAtDesc();
+    List<Noticeboard> findAllByDeleteYn(final char deleteYn, final Sort sort);
 }
