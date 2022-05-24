b2dc4140db70a72b72b262664cd2f0628bb677af
guluming <guluming@naver.com>
2022-05-24 15:08:18 +0900

    페이지 컨트롤러 작성

--- src/main/java/com/sparta/spring_w3_homework/controller/NoticeboardPageController.java
+++ src/main/java/com/sparta/spring_w3_homework/controller/NoticeboardPageController.java
@@ -1,15 +1,23 @@
 package com.sparta.spring_w3_homework.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/board")
 public class NoticeboardPageController {
 
-    @GetMapping("/view")
-    public String openBoardview() {
+    @GetMapping("/view/{id}")
+    public String openNoticeview(@PathVariable Long id, Model model) {
+        model.addAttribute("id", id);
         return "board/view";
     }
+
+//    @GetMapping("/edit")
+//    public String openNoticeedit() {
+//        return "board/edit";
+//    }
 }
