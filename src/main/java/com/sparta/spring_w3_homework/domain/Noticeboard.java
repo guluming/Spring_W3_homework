6e351a0649db3f9bcb994e4e4d45824debcfb43e
guluming <guluming@naver.com>
2022-05-24 15:08:58 +0900

    요청Dto와 응답Dto 분리로 메인 entity 파일 수

--- src/main/java/com/sparta/spring_w3_homework/domain/Noticeboard.java
+++ src/main/java/com/sparta/spring_w3_homework/domain/Noticeboard.java
@@ -1,47 +1,65 @@
 package com.sparta.spring_w3_homework.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
 @Entity
 public class Noticeboard extends Timestamped {
+
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private Long id;
 
-    @Column(nullable = false)
     private String username;
 
-    @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
     private String contents;
 
-    @Column(nullable = false)
     private int password;
 
-    public Noticeboard(String username, String title, String contents, int password) {
+    private char deleteYn;
+
+    private LocalDateTime createdDate = LocalDateTime.now();
+
+    private LocalDateTime modifiedDate = LocalDateTime.now();
+
+    @Builder
+    public Noticeboard(String username, String title, String contents, int password, char deleteYn) {
         this.username = username;
         this.title = title;
         this.contents = contents;
         this.password = password;
+        this.deleteYn = deleteYn;
     }
 
-    public Noticeboard(NoticeboardRequestDto requestDto) {
-        this.username = requestDto.getUsername();
-        this.title = requestDto.getTitle();
-        this.contents = requestDto.getContents();
-        this.password = requestDto.getPassword();
+//    public Noticeboard(NoticeboardRequestDto requestDto) {
+//        this.username = requestDto.getUsername();
+//        this.title = requestDto.getTitle();
+//        this.contents = requestDto.getContents();
+//        this.password = requestDto.getPassword();
+//    }
+
+//    public void update(NoticeboardRequestDto requestDto){
+//        this.username = requestDto.getUsername();
+//        this.title = requestDto.getTitle();
+//        this.contents = requestDto.getContents();
+//    }
+
+    public void update(String username, String title, String contents){
+        this.username = username;
+        this.title = title;
+        this.contents = contents;
+        this.modifiedDate = LocalDateTime.now();
     }
 
-    public void update(NoticeboardRequestDto requestDto){
-        this.username = requestDto.getUsername();
-        this.title = requestDto.getTitle();
-        this.contents = requestDto.getContents();
+    public void delete(){
+        this.deleteYn = 'Y';
     }
 }
