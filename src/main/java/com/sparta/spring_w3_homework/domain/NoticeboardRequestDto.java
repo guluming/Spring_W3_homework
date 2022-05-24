f8712e8039c74cd46ebba4713d87696610773901
guluming <guluming@naver.com>
2022-05-24 15:07:39 +0900

    요청 Dto 작성

--- src/main/java/com/sparta/spring_w3_homework/domain/NoticeboardRequestDto.java
+++ src/main/java/com/sparta/spring_w3_homework/domain/NoticeboardRequestDto.java
@@ -1,11 +1,24 @@
 package com.sparta.spring_w3_homework.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class NoticeboardRequestDto {
     private String username;
     private String title;
     private String contents;
     private int password;
+    private char deleteYn;
+
+    public Noticeboard toEntity(){
+        return Noticeboard.builder()
+                .username(username)
+                .title(title)
+                .contents(contents)
+                .password(password)
+                .deleteYn(deleteYn)
+                .build();
+    }
 }
