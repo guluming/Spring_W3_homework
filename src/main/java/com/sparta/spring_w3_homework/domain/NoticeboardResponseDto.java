19d8381806828de85a4926fca3c06681802a6f37
guluming <guluming@naver.com>
2022-05-25 13:46:17 +0900

    html에 생성일과 마지막수정일 삽입을 위한 변수추가

--- src/main/java/com/sparta/spring_w3_homework/domain/NoticeboardResponseDto.java
+++ src/main/java/com/sparta/spring_w3_homework/domain/NoticeboardResponseDto.java
@@ -5,13 +5,15 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 
 @Getter
-public class NoticeboardResponseDto {
+public class NoticeboardResponseDto extends Timestamped {
 
     private Long id;
     private String username;
     private String title;
     private String contents;
-    private String password;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
 //    private LocalDateTime createdDate;
 //    private LocalDateTime modifiedDate;
 
@@ -20,7 +22,9 @@ public class NoticeboardResponseDto {
         this.username = entity.getUsername();
         this.title = entity.getTitle();
         this.contents = entity.getContents();
-        this.password = entity.getPassword();
+        this.createdAt = entity.getCreatedAt();
+        this.modifiedAt = entity.getModifiedAt();
+
 //        this.createdDate = entity.getCreatedDate();
 //        this.modifiedDate = entity.getModifiedDate();
     }
